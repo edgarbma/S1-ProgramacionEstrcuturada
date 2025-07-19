9c66918fdd2847861dec9c12592151ba801891f0
jorge lara <jorgelara@.com>
2025-07-19 12:16:38 -0600

    ejercicio 7

---
+++ ejercicio7.js
@@ -0,0 +1,13 @@
+x = 10;
+y = "Hola";
+activo = true;
+valorVacio = null;
+persona = {nombre: "Sara"};
+z = 
+
+console.log(typeof x);
+console.log(typeof y);
+console.log(typeof activo);
+console.log(typeof valorVacio);
+console.log(typeof persona);
+console.log(typeof z);
